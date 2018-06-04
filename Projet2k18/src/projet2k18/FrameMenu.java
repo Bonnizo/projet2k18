f585ce4d0ddd20d903642699c2b61be3cef14bf1
Victor <Victor@DESKTOP-41UMU44>
2018-06-04 13:32:56 +0200

    menu

--- Projet2k18/src/projet2k18/FrameMenu.java
+++ Projet2k18/src/projet2k18/FrameMenu.java
@@ -1,15 +1,19 @@
 package projet2k18;
 
+import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Paint;
 import java.awt.Panel;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
@@ -21,6 +25,7 @@ import javax.swing.AbstractButton;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -32,73 +37,125 @@ import projet2k18.RoundedBorder;
 public class FrameMenu extends JFrame {
 
 	public FrameMenu() {
+
 		
-		Icon agenda = new ImageIcon("C:\\Users\\Victor\\Desktop\\ProgJava\\projet2k18\\Image\\IconeMenu\\agenda.jpg");
-		Icon casino = new ImageIcon("C:\\Users\\Victor\\Desktop\\ProgJava\\projet2k18\\Image\\IconeMenu\\casino.png");
-		Icon album = new ImageIcon("C:\\Users\\Victor\\Desktop\\ProgJava\\projet2k18\\Image\\IconeMenu\\album.jpg");
-	
+			//image des icones du menu
+		Icon agenda = new ImageIcon("Image/IconeMenu/contact.png");
+		Icon casino = new ImageIcon("Image/IconeMenu/casino.png");
+		Icon album = new ImageIcon("\\Image\\IconeMenu\\album.jpg");
+
 		
-		
-		
-		
-		
-		
-		
-	
-		
-		
+		//Smartphone solide (contour )
+		BorderLayout ecran = new BorderLayout();
+		JPanel Smartphone = new JPanel();
+		Smartphone.setLayout(ecran);
+		Smartphone.setBackground(Color.BLACK);
+
+		//JFRAME DE DEPART depuis le menu 
 		JFrame FrameMenu = new JFrame("Menu");
 
-		JPanel menu = new JPanel()
+		
+		//menu où il y a les applications 
+		JPanel menu = new JPanel();
 	
-		{
-			protected void paintComponent(Graphics g)
-			{
+		
+		// menu de navigation 
+		JPanel navigation = new JPanel();
+		
+/*
+		{//fond ecran
+			protected void paintComponent(Graphics g) {
 				super.paintComponents(g);
-				ImageIcon fond = new ImageIcon("C:\\Users\\Victor\\Desktop\\ProgJava\\projet2k18\\Image\\FondEcran\\menu2.jpg");
+				ImageIcon fond = new ImageIcon(
+						"C:\\Users\\Victor\\Desktop\\ProgJava\\projet2k18\\Image\\FondEcran\\menu2.jpg");
 				Image fondMenu = fond.getImage();
-				g.drawImage(fondMenu, 0,0, this);
+				g.drawImage(fondMenu, 0, 0, this);
 			}
-		};
+		};*/
 		
 		
-		FlowLayout menuLayout = new FlowLayout(0, 65, 80);
+		//placement navigation
+		navigation.setBounds(10, 570, 400, 150);
+		navigation .setBackground(Color.BLUE);
+		FlowLayout navigationLayout = new FlowLayout(0, 40, 50);
+		menu.setLayout(navigationLayout);
+		
+		//placement menu 
+		menu.setBounds(10, 90, 400, 570);
+		FlowLayout menuLayout = new FlowLayout(0, 40, 50);
 		menu.setLayout(menuLayout);
+		menu.setBackground(Color.WHITE);
 		
-				
+		
+		//creation des boutons des applications  avec photo centrée
 		
 		JButton app1 = new JButton(agenda);
 		app1.setHorizontalTextPosition(AbstractButton.CENTER);
+		app1.setBackground(Color.BLACK);
+		app1.setForeground(Color.BLACK);
 		JButton app2 = new JButton(casino);
 		app2.setHorizontalTextPosition(AbstractButton.CENTER);
+		app2.setBackground(Color.BLACK);
 		JButton app3 = new JButton(album);
 		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app3.setBackground(Color.BLACK);
 		JButton app4 = new JButton(album);
-		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app4.setHorizontalTextPosition(AbstractButton.CENTER);
+		app4.setBackground(Color.BLACK);
 		JButton app5 = new JButton(album);
-		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app5.setHorizontalTextPosition(AbstractButton.CENTER);
+		app5.setBackground(Color.BLACK);
 		JButton app6 = new JButton(album);
-		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app6.setHorizontalTextPosition(AbstractButton.CENTER);
+		app6.setBackground(Color.BLACK);
 		JButton app7 = new JButton(album);
-		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app7.setHorizontalTextPosition(AbstractButton.CENTER);
+		app7.setBackground(Color.BLACK);
 		JButton app8 = new JButton(album);
-		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app8.setHorizontalTextPosition(AbstractButton.CENTER);
+		app8.setBackground(Color.BLACK);
 		JButton app9 = new JButton(album);
-		app3.setHorizontalTextPosition(AbstractButton.CENTER);
+		app9.setHorizontalTextPosition(AbstractButton.CENTER);
+		app9.setBackground(Color.BLACK);
+		JButton app10 = new JButton(album);
+		app10.setHorizontalTextPosition(AbstractButton.CENTER);
+		app10.setBackground(Color.BLACK);
+		JButton app11 = new JButton(album);
+		app11.setHorizontalTextPosition(AbstractButton.CENTER);
+		app11.setBackground(Color.BLACK);
+		JButton app12 = new JButton(album);
+		app12.setHorizontalTextPosition(AbstractButton.CENTER);
+		app12.setBackground(Color.BLACK);
 		
-	
+		//creation bouton navigation
+		
+		
+		
+		
+		//bouton d'accueil 
+		JButton bouton = new JButton() {
+			protected void paintComponent(Graphics g) 
+			{ g.setColor(getBackground());
+			g.fillOval(400, 300, 200, 100);
+			super.paintComponent(g); }
+		};
+		
 
-
-		app1.setPreferredSize(new Dimension(70, 70));
-		app2.setPreferredSize(new Dimension(70, 70));
-		app3.setPreferredSize(new Dimension(70, 70));
-		app4.setPreferredSize(new Dimension(70, 70));
-		app5.setPreferredSize(new Dimension(70, 70));
-		app6.setPreferredSize(new Dimension(70, 70));
-		app7.setPreferredSize(new Dimension(70, 70));
-		app8.setPreferredSize(new Dimension(70, 70));
-		app9.setPreferredSize(new Dimension(70, 70));
-
+		//redimension bouton
+		app1.setPreferredSize(new Dimension(80, 80));
+		app2.setPreferredSize(new Dimension(80, 80));
+		app3.setPreferredSize(new Dimension(80, 80));
+		app4.setPreferredSize(new Dimension(80, 80));
+		app5.setPreferredSize(new Dimension(80, 80));
+		app6.setPreferredSize(new Dimension(80, 80));
+		app7.setPreferredSize(new Dimension(80, 80));
+		app8.setPreferredSize(new Dimension(80, 80));
+		app9.setPreferredSize(new Dimension(80, 80));
+		app10.setPreferredSize(new Dimension(80, 80));
+		app11.setPreferredSize(new Dimension(80, 80));
+		app12.setPreferredSize(new Dimension(80, 80));
+		
+		// arrondir bouton !--> comment enlever ce quil depasse ??<--!
 		app1.setBorder(new RoundedBorder(20));
 		app2.setBorder(new RoundedBorder(20));
 		app3.setBorder(new RoundedBorder(20));
@@ -109,8 +166,7 @@ public class FrameMenu extends JFrame {
 		app8.setBorder(new RoundedBorder(20));
 		app9.setBorder(new RoundedBorder(20));
 
-	
-		
+		//ajout bouton
 		menu.add(app1);
 		menu.add(app2);
 		menu.add(app3);
@@ -120,21 +176,29 @@ public class FrameMenu extends JFrame {
 		menu.add(app7);
 		menu.add(app8);
 		menu.add(app9);
+		menu.add(app10);
+		menu.add(app11);
+		menu.add(app12);
 		
+/*			//ajout bouton menu
+	
+		Smartphone.add(bouton, BorderLayout.SOUTH);
+*/
 		
-		
-		
+		//ajout des panels sur frame
 		
 		FrameMenu.add(menu);
-		FrameMenu.setBackground(Color.red);
+		FrameMenu.add(navigation);
+		FrameMenu.add(Smartphone);
+	
+		
+		//frame caractéristiques
 		FrameMenu.setResizable(false);
-		FrameMenu.setSize(480, 800);
+		FrameMenu.setSize(420, 800);
 		FrameMenu.setUndecorated(true);
-		FrameMenu.setShape(new RoundRectangle2D.Double(2, 2, 480, 800, 100, 100));
+		FrameMenu.setShape(new RoundRectangle2D.Double(2, 2, 420, 800, 100, 100));
 		FrameMenu.setVisible(true);
-
+		FrameMenu.setLocation(450, 100);
 	}
-	
-	
 
 }
