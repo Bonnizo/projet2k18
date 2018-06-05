48cc231ed6bd0dc5e8d8bc8861f467020ac116d5
Nathan <Nathan@LAPTOP-L5JQPD63>
2018-06-05 13:03:24 +0200

    fdp

---
+++ Projet2k18/src/projet2k18/GalleryPanel.java
@@ -0,0 +1,12 @@
+package projet2k18;
+
+import javax.swing.JPanel;
+
+public class GalleryPanel extends JPanel{
+
+	public GalleryPanel() {
+		// TODO Auto-generated constructor stub
+		//fdp
+	}
+
+}
---
+++ Projet2k18/src/projet2k18/Photo.java
@@ -0,0 +1,9 @@
+package projet2k18;
+
+public class Photo {
+
+	public Photo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
