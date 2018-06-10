28ca34b2c867eda9decced89357c390e3363442c
Nathan <Nathan@LAPTOP-L5JQPD63>
2018-06-10 17:49:31 +0200

    Galerie : ajout de photo possible

--- Projet2k18/src/projet2k18/GalleryPanel.java
+++ Projet2k18/src/projet2k18/GalleryPanel.java
@@ -10,6 +10,9 @@ import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -17,9 +20,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JFileChooser;
 
-public class GalleryPanel extends JPanel{
-	
+public class GalleryPanel extends JPanel {
+
 	private ImageIcon imgIcon;
 	private File[] listOfFiles;
 	private JLabel[] imagesList;
@@ -38,33 +43,36 @@ public class GalleryPanel extends JPanel{
 	private JPanel panel_1;
 	private JLabel lblTrash;
 	private JScrollPane scroll;
-	
+	private JPanel SouthPanelGallery;
+	private JLabel lblAdd;
+	private JFileChooser fileChooser = new JFileChooser();
+
 	public GalleryPanel(CardLayout cardLayout, JPanel contentPanel) {
-		
+
 		this.cardLayout = cardLayout;
 		this.contentPanel = contentPanel;
 		setLayout(new BorderLayout());
 		container.setLayout(clGalerie);
 		refillGrid();
-		
+
 		setGalleryMenuPanel();
-		    
+
 	}
-	
+
 	private void refillGrid() {
 		// TODO Auto-generated constructor stub
 		gImage = new GridImage();
-		
+
 		scroll = new JScrollPane(gImage);
 		container.add(scroll);
-		add(container,BorderLayout.CENTER);
+		add(container, BorderLayout.CENTER);
 	}
-	
+
 	private void setGalleryMenuPanel() {
 		panel = new JPanel();
 		add(panel, BorderLayout.NORTH);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		lblGalerie = new JLabel("Galerie");
 		lblGalerie.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGalerie.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -74,63 +82,75 @@ public class GalleryPanel extends JPanel{
 		panel.add(lblRetour, BorderLayout.WEST);
 		lblRetour.addMouseListener(new Menu());
 		lblRetour.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		
+
 		panel_1 = new JPanel();
 		panel.add(panel_1, BorderLayout.EAST);
-		
+
 		lblTrash = new JLabel("");
 		lblTrash.addMouseListener(new Menu());
 		panel_1.add(lblTrash);
 		lblSelect = new JLabel("Select  ");
 		panel_1.add(lblSelect);
 		lblSelect.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+		SouthPanelGallery = new JPanel();
+		add(SouthPanelGallery, BorderLayout.SOUTH);
+
+		icon = new ImageIcon("image/plus.png");
+		lblAdd = new JLabel(icon);
+		lblAdd.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAdd.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		lblAdd.addMouseListener(new Menu());
+		SouthPanelGallery.add(lblAdd);
+
 		lblSelect.addMouseListener(new Menu());
-		
+
 	}
-	
+
 	class SelectImage extends MouseAdapter {
 		public void mouseClicked(MouseEvent e) {
-			if(isSelected) {
-				
-				temp =  (JLabel)e.getSource();
-				if(temp.isEnabled()) {
+			if (isSelected) {
+
+				temp = (JLabel) e.getSource();
+				if (temp.isEnabled()) {
 					temp.setEnabled(false);
-				}
-				else {
+				} else {
 					temp.setEnabled(true);
 				}
 				temp.setForeground(Color.BLACK);
 				temp.setText("Deleted");
 			} else {
-				
+
 			}
 		}
 	}
-	
+
 	class Menu extends MouseAdapter {
 		public void mouseClicked(MouseEvent e) {
-			if(e.getSource().equals(lblSelect)) {
-				if(!isSelected) {
+			if (e.getSource().equals(lblSelect)) {
+				if (!isSelected) {
 					isSelected = true;
 					lblSelect.setText("selection  ");
 					icon = new ImageIcon("image/trash.png");
 					lblTrash.setIcon(icon);
 					lblTrash.setCursor(new Cursor(Cursor.HAND_CURSOR));
-				}
-				else if(isSelected) {
+				} else if (isSelected) {
 					isSelected = false;
 					lblSelect.setText("select  ");
 					lblTrash.setIcon(null);
-					
+					for (int i = 0; i < imagesList.length; i++) {
+						if (!imagesList[i].isEnabled()) {
+							imagesList[i].setEnabled(true);
+						}
+					}
+
 				}
-			}
-			else if(e.getSource().equals(lblRetour)) {
+			} else if (e.getSource().equals(lblRetour)) {
 				cardLayout.show(contentPanel, "menu");
 				isSelected = false;
-			}
-			else if(e.getSource().equals(lblTrash)) {
-				for(int i = 0; i < imagesList.length; i++) {
-					if(!imagesList[i].isEnabled()) {
+			} else if (e.getSource().equals(lblTrash)) {
+				for (int i = 0; i < imagesList.length; i++) {
+					if (!imagesList[i].isEnabled()) {
 						File deletedFile = new File(imagesList[i].getIcon().toString());
 						deletedFile.delete();
 					}
@@ -141,40 +161,113 @@ public class GalleryPanel extends JPanel{
 				refillGrid();
 				revalidate();
 				repaint();
+			} else if (e.getSource().equals(lblAdd)) {
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PNG Images", "jpg", "jpeg", "png");
+
+				fileChooser.setFileFilter(filter);
+				fileChooser.setMultiSelectionEnabled(true);
+				int reponse = fileChooser.showOpenDialog(null);
+				if (reponse == fileChooser.APPROVE_OPTION) {
+
+					File[] fs = fileChooser.getSelectedFiles();
+					String location = "Photo\\";
+					int cptExistantImage = 0;
+					for (int i = 0; i < fs.length; i++) {
+						String path = location + fs[i].getName();
+						File destination;
+
+						Path source = fs[i].toPath();
+
+						if (checkExtension(fs[i]) == true) {
+							String choosedFile = fs[i].getName().substring(0, fs[i].getName().lastIndexOf("."));
+							for (int j = 0; j < imagesList.length; j++) {
+								String existingFilename = imagesList[j].getIcon().toString();
+								existingFilename = existingFilename.substring(location.length(),
+										existingFilename.lastIndexOf("."));
+								if (choosedFile.equals(existingFilename) == true) {
+									cptExistantImage++;
+								}
+
+							}
+							if (cptExistantImage > 0) {
+								String choosedFileExt = getFileExtension(fs[i]);
+								destination = new File(
+										location + choosedFile + "(" + cptExistantImage + ")." + choosedFileExt);
+							} else {
+								destination = new File(path);
+							}
+							try {
+								System.out.println(source);
+								Files.copy(source, destination.toPath());
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							}
+
+						} else {
+							break;
+						}
+					}
+					gImage = null;
+					container.remove(scroll);
+					remove(container);
+					refillGrid();
+					revalidate();
+					repaint();
+				}
+				if (reponse == fileChooser.CANCEL_OPTION) {
+					fileChooser.cancelSelection();
+					return;
+				}
 			}
-			
+
 		}
-		
+
 	}
-	
+
+	private String getFileExtension(File file) {
+		String fileName = file.getName();
+		if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+			return fileName.substring(fileName.lastIndexOf(".") + 1);
+		else
+			return "";
+	}
+
+	private boolean checkExtension(File fichier) {
+		String ext = getFileExtension(fichier);
+		if (ext.toLowerCase().equals("jpeg") || ext.toLowerCase().equals("jpg") || ext.toLowerCase().equals("png"))
+			return true;
+		else
+			return false;
+	}
+
 	class GridImage extends JPanel {
-		
+
 		public GridImage() {
-			setLayout(new GridLayout(0,3));
+			setLayout(new GridLayout(0, 3));
 			fillGrid();
 		}
-		
+
 		private void fillGrid() {
 			File folder = new File("Photo");
 			listOfFiles = folder.listFiles();
 			imagesList = new JLabel[listOfFiles.length];
-		    for (int i = 0; i < listOfFiles.length; i++) {
+			for (int i = 0; i < listOfFiles.length; i++) {
 				if (listOfFiles[i].isFile()) {
 					imagesList[i] = new JLabel();
 					imagesList[i].setPreferredSize(new Dimension(120, 120));
 					imagesList[i].addMouseListener(new SelectImage());
 					add(imagesList[i]);
 				}
-		    }
+			}
 		}
-		
+
 		public void paintComponent(Graphics g) {
-	        super.paintComponent(g);
-	        for (int i = 0; i < listOfFiles.length; i++) {
-		        imgIcon = new ImageIcon(listOfFiles[i].getPath());
-		        imagesList[i].setIcon(imgIcon);
-	        }
-	    }
+			super.paintComponent(g);
+			for (int i = 0; i < listOfFiles.length; i++) {
+				imgIcon = new ImageIcon(listOfFiles[i].getPath());
+				imagesList[i].setIcon(imgIcon);
+			}
+		}
 	}
 
 }
