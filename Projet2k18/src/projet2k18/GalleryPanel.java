36e073c74e7a29c562b24523e9c5d685a5315a46
Nathan <Nathan@LAPTOP-L5JQPD63>
2018-06-10 16:23:23 +0200

    Suppression Image de la galerie

--- Projet2k18/src/projet2k18/GalleryPanel.java
+++ Projet2k18/src/projet2k18/GalleryPanel.java
@@ -2,20 +2,16 @@ package projet2k18;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.io.File;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -23,12 +19,12 @@ import java.awt.Font;
 import javax.swing.SwingConstants;
 
 public class GalleryPanel extends JPanel{
-	private ImageIcon fdp;
+	
+	private ImageIcon imgIcon;
 	private File[] listOfFiles;
 	private JLabel[] imagesList;
 	private GridImage gImage;
 	private JPanel container = new JPanel();
-	String []listContent = {"Card1", "Card2"};
 	private CardLayout clGalerie = new CardLayout();
 	private JPanel panel;
 	private JLabel lblGalerie;
@@ -38,23 +34,33 @@ public class GalleryPanel extends JPanel{
 	private CardLayout cardLayout;
 	private JPanel contentPanel;
 	private Boolean isSelected = false;
+	private JLabel temp;
+	private JPanel panel_1;
+	private JLabel lblTrash;
+	private JScrollPane scroll;
 	
 	public GalleryPanel(CardLayout cardLayout, JPanel contentPanel) {
 		
 		this.cardLayout = cardLayout;
 		this.contentPanel = contentPanel;
-		
-		
 		setLayout(new BorderLayout());
-		
 		container.setLayout(clGalerie);
+		refillGrid();
+		
+		setGalleryMenuPanel();
+		    
+	}
+	
+	private void refillGrid() {
+		// TODO Auto-generated constructor stub
 		gImage = new GridImage();
 		
-		JScrollPane scroll = new JScrollPane(gImage);
-		container.add(scroll, listContent[0]);
-		// TODO Auto-generated constructor stub
-		this.add(container,BorderLayout.CENTER);
-		
+		scroll = new JScrollPane(gImage);
+		container.add(scroll);
+		add(container,BorderLayout.CENTER);
+	}
+	
+	private void setGalleryMenuPanel() {
 		panel = new JPanel();
 		add(panel, BorderLayout.NORTH);
 		panel.setLayout(new BorderLayout(0, 0));
@@ -66,31 +72,75 @@ public class GalleryPanel extends JPanel{
 		icon = new ImageIcon("image/back.png");
 		lblRetour = new JLabel(icon);
 		panel.add(lblRetour, BorderLayout.WEST);
-		lblRetour.addMouseListener(new returnMenu());
+		lblRetour.addMouseListener(new Menu());
 		lblRetour.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		lblSelect = new JLabel("Select");
-		lblSelect.addMouseListener(new returnMenu());
+		
+		panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.EAST);
+		
+		lblTrash = new JLabel("");
+		lblTrash.addMouseListener(new Menu());
+		panel_1.add(lblTrash);
+		lblSelect = new JLabel("Select  ");
+		panel_1.add(lblSelect);
 		lblSelect.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		panel.add(lblSelect, BorderLayout.EAST);
-		    
+		lblSelect.addMouseListener(new Menu());
+		
 	}
 	
+	class SelectImage extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
+			if(isSelected) {
+				
+				temp =  (JLabel)e.getSource();
+				if(temp.isEnabled()) {
+					temp.setEnabled(false);
+				}
+				else {
+					temp.setEnabled(true);
+				}
+				temp.setForeground(Color.BLACK);
+				temp.setText("Deleted");
+			} else {
+				
+			}
+		}
+	}
 	
-	class returnMenu extends MouseAdapter {
+	class Menu extends MouseAdapter {
 		public void mouseClicked(MouseEvent e) {
 			if(e.getSource().equals(lblSelect)) {
 				if(!isSelected) {
 					isSelected = true;
-					lblSelect.setText("selection");
+					lblSelect.setText("selection  ");
+					icon = new ImageIcon("image/trash.png");
+					lblTrash.setIcon(icon);
+					lblTrash.setCursor(new Cursor(Cursor.HAND_CURSOR));
 				}
 				else if(isSelected) {
 					isSelected = false;
-					lblSelect.setText("select");
+					lblSelect.setText("select  ");
+					lblTrash.setIcon(null);
+					
 				}
 			}
 			else if(e.getSource().equals(lblRetour)) {
 				cardLayout.show(contentPanel, "menu");
 				isSelected = false;
+			}
+			else if(e.getSource().equals(lblTrash)) {
+				for(int i = 0; i < imagesList.length; i++) {
+					if(!imagesList[i].isEnabled()) {
+						File deletedFile = new File(imagesList[i].getIcon().toString());
+						deletedFile.delete();
+					}
+				}
+				gImage = null;
+				container.remove(scroll);
+				remove(container);
+				refillGrid();
+				revalidate();
+				repaint();
 			}
 			
 		}
@@ -110,9 +160,9 @@ public class GalleryPanel extends JPanel{
 			imagesList = new JLabel[listOfFiles.length];
 		    for (int i = 0; i < listOfFiles.length; i++) {
 				if (listOfFiles[i].isFile()) {
-					System.out.println("File " + listOfFiles[i].getName());
 					imagesList[i] = new JLabel();
 					imagesList[i].setPreferredSize(new Dimension(120, 120));
+					imagesList[i].addMouseListener(new SelectImage());
 					add(imagesList[i]);
 				}
 		    }
@@ -121,8 +171,8 @@ public class GalleryPanel extends JPanel{
 		public void paintComponent(Graphics g) {
 	        super.paintComponent(g);
 	        for (int i = 0; i < listOfFiles.length; i++) {
-		        fdp = new ImageIcon(listOfFiles[i].getPath());
-		        imagesList[i].setIcon(fdp);
+		        imgIcon = new ImageIcon(listOfFiles[i].getPath());
+		        imagesList[i].setIcon(imgIcon);
 	        }
 	    }
 	}
