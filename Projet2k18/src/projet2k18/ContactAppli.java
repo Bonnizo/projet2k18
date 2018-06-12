e209e85567226d641becb0fd19bd5b98737b085c
Nathan Bovier <nathanbovier@hotmail.com>
2018-06-12 11:54:26 +0200

    Création nouveaau contact

--- Projet2k18/src/projet2k18/ContactAppli.java
+++ Projet2k18/src/projet2k18/ContactAppli.java
@@ -18,7 +18,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -35,11 +38,7 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 import projet2k18.FrameMain.boutonContact;
 import projet2k18.PersonneInfo;
 
-public class ContactAppli extends JPanel{
-
-	
-	
-	
+public class ContactAppli extends JPanel {
 
 	// panel contact
 	private JPanel contactPanel = new JPanel();
@@ -48,9 +47,7 @@ public class ContactAppli extends JPanel{
 	private JPanel imgPanel = new JPanel();
 	private JPanel espacePanel1 = new JPanel();
 	private JPanel espacePanel2 = new JPanel();
-	
-	
-	
+
 	// Texfield
 	private static HintTextField prenom = new HintTextField("prenom");
 	private static HintTextField nom = new HintTextField("nom");
@@ -58,162 +55,122 @@ public class ContactAppli extends JPanel{
 	private static HintTextField adresse = new HintTextField("adresse");
 	private static HintTextField email = new HintTextField("email");
 	private static HintTextField prenomDefaut = new HintTextField("prenom");
-	private static HintTextField nomDefaut= new HintTextField("nom");
+	private static HintTextField nomDefaut = new HintTextField("nom");
 	private static HintTextField telephoneDefaut = new HintTextField("telephone");
 	private static HintTextField adresseDefaut = new HintTextField("adresse");
 	private static HintTextField emailDefaut = new HintTextField("email");
 	
-	
-	
-	
-	
-	//image
-	ImageIcon back = new ImageIcon("image/back.png");	
+	private Calendar now = Calendar.getInstance();
+	DateFormat hhmm = new SimpleDateFormat("HH:mm");
+
+	// image
+	ImageIcon back = new ImageIcon("image/back.png");
 	ImageIcon ok = new ImageIcon("image/correct.png");
-	
-	//bouton 
-	
-	
+
+	// bouton
 	private BoutonMenu annuler = new BoutonMenu(back, 40, new RetourContact());
 	private BoutonMenu correct = new BoutonMenu(ok, 40, new AjouterContact());
 
-	
-
-	
-	
-	
-	//layout
-	private GridLayout grille = new GridLayout(0,1, 20,20);	
-	private FlowLayout boutonPlacer = new FlowLayout(100,100,80);
+	// layout
+	private GridLayout grille = new GridLayout(0, 1, 20, 20);
+	private FlowLayout boutonPlacer = new FlowLayout(100, 100, 80);
 	private BorderLayout centre = new BorderLayout();
-	
-	
-	
-	
-	//cardlayout
+
+	// cardlayout
 	private CardLayout cardLayout2;
-	private JPanel contentPanel2; 
-	
-		
-	//Création des contacts
-	//liste utilisée pour la serialization
-	public ArrayList <PersonneInfo> PersonneListe = new ArrayList<PersonneInfo>();
-	
-		
-		
-		
-		
-		
-		public  ContactAppli(CardLayout cardLayout2, JPanel contentPanel2){
+	private JPanel contentPanel2;
+
+	// Création des contacts
+	// liste utilisée pour la serialization
+	public ArrayList<PersonneInfo> PersonneListe = new ArrayList<PersonneInfo>();
+
+	public ContactAppli(CardLayout cardLayout2, JPanel contentPanel2) {
 		this.cardLayout2 = cardLayout2;
 		this.contentPanel2 = contentPanel2;
-		//caractristiques page
-		
+		// caractristiques page
+
 		setLayout(centre);
 		setBackground(Color.WHITE);
-		
 
-		//taille panel dans le container
-		
-		infoPanel.setPreferredSize(new Dimension( 150, 470));
-		
-		boutonPanel.setPreferredSize(new Dimension(300,140));
+		// taille panel dans le container
 
-		imgPanel.setPreferredSize(new Dimension(100,100));
-		
-		espacePanel1.setPreferredSize(new Dimension(10,500));
-		espacePanel2.setPreferredSize(new Dimension (400,30));
-				
-		//bordure label dans panel
-		
-		Border panelBorder = BorderFactory.createMatteBorder(0, 0, 1,0 , Color.BLACK);
-		
+		infoPanel.setPreferredSize(new Dimension(150, 470));
+
+		boutonPanel.setPreferredSize(new Dimension(300, 140));
+
+		imgPanel.setPreferredSize(new Dimension(100, 100));
+
+		espacePanel1.setPreferredSize(new Dimension(10, 500));
+		espacePanel2.setPreferredSize(new Dimension(400, 30));
+
+		// bordure label dans panel
+
+		Border panelBorder = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK);
+
 		infoPanel.add(prenom);
 		prenom.setBorder(panelBorder);
 		infoPanel.add(nom);
 		nom.setBorder(panelBorder);
-		infoPanel.add(telephone);	
+		infoPanel.add(telephone);
 		telephone.setBorder(panelBorder);
 		infoPanel.add(adresse);
 		adresse.setBorder(panelBorder);
-		infoPanel.add(email);	
+		infoPanel.add(email);
 		email.setBorder(panelBorder);
-		
-		
-	
-	
+
 		boutonPanel.add(annuler);
 		boutonPanel.add(correct);
-		
-		
-		
-		
-		
-		
-		//layout des 2 panels info + bouton
+
+		// layout des 2 panels info + bouton
 		infoPanel.setLayout(grille);
 		boutonPanel.setLayout(boutonPlacer);
-		
-		//transparent panel
+
+		// transparent panel
 		infoPanel.setOpaque(false);
 		boutonPanel.setOpaque(false);
 		imgPanel.setOpaque(false);
 		espacePanel1.setOpaque(false);
 		espacePanel2.setOpaque(false);
-		
-		
-		
-		
-		
-		
-		//rajouter panel
-		add(infoPanel,BorderLayout.CENTER);
-		add(boutonPanel,BorderLayout.SOUTH);
+
+		// rajouter panel
+		add(infoPanel, BorderLayout.CENTER);
+		add(boutonPanel, BorderLayout.SOUTH);
 		add(imgPanel, BorderLayout.WEST);
 		add(espacePanel1, BorderLayout.EAST);
 		add(espacePanel2, BorderLayout.NORTH);
-		
-	
-	
-		
-		
-		 
-		
-		
-		
-	}
-	
 
-	
-	class AjouterContact  extends Listener
-	{
+	}
+
+	class AjouterContact extends Listener {
+		public void actionPerformed(ActionEvent e) {
+			PersonneInfo person = new PersonneInfo(prenom.getText(), nom.getText(), telephone.getText(), adresse.getText(), email.getText());
+			try {
+				FileOutputStream out = new FileOutputStream("SerialisationContact/Contact" + prenom.getText() + "_" + nom.getText()+ now.getTimeInMillis()+".ser");
+				ObjectOutputStream oos = new ObjectOutputStream(out);
+				oos.writeObject(person);
+
+				oos.close();
+			} catch (IOException f) {
+				// …
+			}
+			
+			cardLayout2.show(contentPanel2, "contactPanel");
+		}
+	}
+
+	class RetourContact extends Listener {
+		@Override
 		public void actionPerformed(ActionEvent e) {
 			cardLayout2.show(contentPanel2, "contactPanel");
-			
+			// prenom.setText(prenomDefaut.getText());
+			nom.setText(nomDefaut.getText());
+			telephone.setText(telephoneDefaut.getText());
+			adresse.setText(adresseDefaut.getText());
+			email.setText(emailDefaut.getText());
+
 		}
 	}
-
-	
-		
-	
-	
-	
-	class RetourContact extends Listener 
-	{
-		@Override
-		public void actionPerformed(ActionEvent e) 
-		{
-			cardLayout2.show(contentPanel2,  "contactPanel");
-			 //prenom.setText(prenomDefaut.getText()); 
-			 nom.setText(nomDefaut.getText());
-			 telephone.setText(telephoneDefaut.getText()); 
-			 adresse.setText(adresseDefaut.getText()); 
-			email.setText(emailDefaut.getText()); 
-			
-			
-		}
-	}
-
+	/*
 	public static void infoTransfert(PersonneInfo info1) {
 		// TODO Auto-generated method stub
 		String prenomFinal = prenom.getText();
@@ -221,11 +178,11 @@ public class ContactAppli extends JPanel{
 		String telephoneFinal = telephone.getText();
 		String adresseFinal = adresse.getText();
 		String emailFinal = email.getText();
-		
+
 		info1.prenom = prenomFinal;
-		info1.nom = nomFinal ;
+		info1.nom = nomFinal;
 		info1.telephone = telephoneFinal;
-		info1.adresse = adresseFinal ;
-		info1.email= emailFinal;
-	}
+		info1.adresse = adresseFinal;
+		info1.email = emailFinal;
+	}*/
 }
--- Projet2k18/src/projet2k18/ContactPanel.java
+++ Projet2k18/src/projet2k18/ContactPanel.java
@@ -3,11 +3,13 @@ package projet2k18;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -25,19 +27,17 @@ import javax.swing.border.Border;
 
 import projet2k18.ContactAppli;
 import projet2k18.PersonneInfo;
+import projet2k18.GalleryPanel.SelectImage;
 
 public class ContactPanel extends JPanel {
 
-	//image des boutons 
+	// image des boutons
 	private ImageIcon rajouterContact = new ImageIcon("image/plus.png");
 	private ImageIcon effacerContact = new ImageIcon("image/close.png");
-	
-	
-	//image contact
+
+	// image contact
 	private ImageIcon contactPhoto = new ImageIcon("image/close.png");
-	
-	
-	
+
 	// bouton contactPanel
 
 	private BoutonMenu plus = new BoutonMenu(rajouterContact, 40, new boutonContact());
@@ -46,10 +46,7 @@ public class ContactPanel extends JPanel {
 	// panel contactPanel
 	private JPanel contactPanel = new JPanel();
 
-	
-		
-	
-	// panel liste deroulante de contact 
+	// panel liste deroulante de contact
 	private JPanel annuaire = new JPanel();
 	private JScrollPane listeContacts = new JScrollPane(annuaire);
 
@@ -57,156 +54,107 @@ public class ContactPanel extends JPanel {
 
 	private JPanel boutonContact = new JPanel();
 
-	
-
-	
-	
-	
 	// cardlayout contact
-		private CardLayout cardLayout2 = new CardLayout();
-		private JPanel contentPanel2 = new JPanel(cardLayout2);
+	private CardLayout cardLayout2 = new CardLayout();
+	private JPanel contentPanel2 = new JPanel(cardLayout2);
 
-		private ContactAppli contactAppli = new ContactAppli(cardLayout2, contentPanel2);
-		
-		
-		
-		
-		// grid layout, agencement colonne contact + photo
-		private GridLayout layoutContact = new GridLayout(0,1);
-	
-		
-		//document contact
-		
-		//liste definitive
-	 ArrayList <PersonneInfo> enCours = new ArrayList <PersonneInfo>();
-	 ArrayList <PersonneInfo> finale = new ArrayList <PersonneInfo>();
-		
-	 
-	
-		
+	private ContactAppli contactAppli = new ContactAppli(cardLayout2, contentPanel2);
+
+	// grid layout, agencement colonne contact + photo
+	private GridLayout layoutContact = new GridLayout(0, 1);
+
+	// document contact
+
+	// liste definitive
+	/*ArrayList<PersonneInfo> enCours = new ArrayList<PersonneInfo>();
+	ArrayList<PersonneInfo> finale = new ArrayList<PersonneInfo>();*/
+	private PersonneInfo[] listPerson;
+
 	public ContactPanel() {
-		//couleur base
+		// couleur base
 		contactPanel.setBackground(Color.WHITE);
 		annuaire.setLayout(layoutContact);
-		
-		
-		//panel organise cardlayout
+
+		// panel organise cardlayout
 		this.add(contentPanel2);
 
-		
-		//ajoute du panel des contacts et pour ajouter des contacts
+		// ajoute du panel des contacts et pour ajouter des contacts
 		contentPanel2.add(contactPanel, "contactPanel");
 		contentPanel2.add(contactAppli, "contactAppli");
-		
-	
-		//panel des boutons
+
+		// panel des boutons
 		boutonContact.setBackground(Color.WHITE);
 		boutonContact.setPreferredSize(new Dimension(400, 670));
 		boutonContact.add(plus);
 		boutonContact.add(supprimer);
 
-		
-		
-		//panel avec scrollbar pour liste contact
+		// panel avec scrollbar pour liste contact
 		listeContacts.setPreferredSize(new Dimension(400, 670));
 		listeContacts.setBackground(Color.WHITE);
-		
-		
-	
-		
-		
-		
-	
-		
+		listeContacts.getVerticalScrollBar().setUnitIncrement(16);
 
 		Border panelBorder = BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK);
-	
-		
-		
-		
-		
-		
-		
-	
-		
-		
-		//ajout de la liste de contact sur les boutons comme ça ils sont fixes meme si il y a un scroll
+
+		// ajout de la liste de contact sur les boutons comme ça ils sont fixes meme si
+		// il y a un scroll
 		boutonContact.add(listeContacts, BorderLayout.CENTER);
-		//ajout des du panel bouton 
+		// ajout des du panel bouton
 		contactPanel.add(boutonContact, BorderLayout.NORTH);
-
 		
-		//ajouter contact list 
-
-		PersonneInfo info1 =new PersonneInfo(null,null,null,null,null);
-		ContactAppli.infoTransfert(info1);
-		PersonneInfo info2 =new PersonneInfo("bw333blba","blablba","blablba","blablba","blablba");
-		PersonneInfo info3 =new PersonneInfo("bw333blba","blablba","blablba","blablba","blablba");
-		PersonneInfo info4 =new PersonneInfo("bw333blba","blablba","blablba","blablba","blablba");
-		PersonneInfo info5 =new PersonneInfo("bw333blba","blablba","blablba","blablba","blablba");
-		enCours.add(info1);
-		enCours.add(info2);	
-		enCours.add(info3);
-		enCours.add(info4);
-		enCours.add(info5);	
-		try {
-			 FileOutputStream out = new FileOutputStream("Contact.ser" );
-			 ObjectOutputStream oos = new ObjectOutputStream( out );
-			for(int i = 0; i<enCours.size();i++) {
-			 oos.writeObject(enCours.get(i) );}
-			
-			 oos.close();
-			} catch(IOException f) {
-			 // … 
-	}
-		try {
-			 FileInputStream in = new FileInputStream("Contact.ser" );
-			 ObjectInputStream ois = new ObjectInputStream( in );
-			
-			 for (int i = 0; i<enCours.size();i++) {
-			 PersonneInfo d = (PersonneInfo) ois.readObject();
-			 finale.add(d);
-			 }
-			 ois.close();
-			} catch(IOException e) {
-			 // …
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		File folder = new File("SerialisationContact");
+		File[] listOfFiles = folder.listFiles();
+		
+		listPerson = new PersonneInfo[listOfFiles.length];
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				System.out.println(listOfFiles[i]);
+				try {
+					FileInputStream in = new FileInputStream(listOfFiles[i]);
+					ObjectInputStream ois = new ObjectInputStream(in);
+					//PersonneInfo temp = (PersonneInfo)ois;
+					try {
+						listPerson[i] = (PersonneInfo)ois.readObject();
+					} catch (ClassNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					ois.close();
+				} catch (IOException e) {
+					// …
+				}
+				
 			}
-		
-		for(int i =0 ; i<finale.size(); i++) {
-		JLabel test = new JLabel(finale.get(i).getPrenom()+" "+finale.get(i).getNom()+" "+finale.get(i).getTelephone()+" "+finale.get(i).getAdresse()+" "+finale.get(i).getEmail()+" ");
-		test.setPreferredSize(new Dimension(300,400));
-		annuaire.add(test);
 		}
 		
-		
-		
+		/*
+		for (int i = 0; i < finale.size(); i++) {
+			JLabel test = new JLabel(
+					finale.get(i).getPrenom() + " " + finale.get(i).getNom() + " " + finale.get(i).getTelephone() + " "
+							+ finale.get(i).getAdresse() + " " + finale.get(i).getEmail() + " ");
+			test.setPreferredSize(new Dimension(300, 400));
+			annuaire.add(test);
+		}
+		*/
 		listeContacts.repaint();
 		listeContacts.revalidate();
 		annuaire.repaint();
 		annuaire.revalidate();
-		
+
 	}
-	
-	
 
 	class boutonContact extends Listener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			cardLayout2.show(contentPanel2, "contactAppli");
-			
-			
+
 		}
+
 		public void actionPerformed2(ActionEvent f) {
-			PersonneInfo info5 =new PersonneInfo("bw344444","blablba","blablba","blablba","blablba");
-			enCours.add(info5);
-			
-			
+			PersonneInfo info5 = new PersonneInfo("bw344444", "blablba", "blablba", "blablba", "blablba");
+			//enCours.add(info5);
+
 		}
-		
-		
+
 	}
 
 	class effacerContact extends Listener {
